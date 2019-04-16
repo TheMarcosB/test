3d16a3329e686335dbcc4307b24284bce3fbb9c6
Marcos Bento <themarcosbc@gmail.com>
2019-04-16 19:11:34 -0300

    Delete set_test.py

--- set_test.py
+++
@@ -1,8 +0,0 @@
-from lib import Set
-
-start = ('-' * 50) + '\n//'
-center = '\n' + ('-' * 50) + '\n'
-end = '\n'
-
-print(start, 'date', center, Time.date(), end)
-print(start, 'web',  center, Time.web(),  end)
